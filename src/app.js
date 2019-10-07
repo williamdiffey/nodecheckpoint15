0ca089a393d5e023678fbeefed8322386f8c5e92
William Diffey <50226488+williamdiffey@users.noreply.github.com>
2019-10-07 12:17:12 +0900

    Changes to be committed:     new file:   info.log     new file:   migrations/001.do.create_blogful_articles.sql     new file:   migrations/001.undo.create_blogful_articles.sql     new file:   migrations/002.do.alter_article_style.sql     new file:   migrations/002.undo.alter_article_style.sql     modified:   package-lock.json     modified:   package.json     new file:   postgrator-config.js     new file:   seeds/seed.blogful_articles.sql     modified:   src/app.js     new file:   src/articles-service.js     modified:   src/config.js     modified:   src/server.js     new file:   test/articles-endpoints.spec.js     modified:   test/setup.js

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 const validateBearerToken = require('./validate-bearer-token')
 const errorHandler = require('./error-handler')
-const boilerplateRouter = require('./boilerplate-router/bookmarks-router')
+// const boilerplateRouter = require('./boilerplate-router/bookmarks-router')
 
 
 const app = express()
@@ -14,14 +15,30 @@ const app = express()
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
   skip: () => NODE_ENV === 'test'
 }))
+// app.use(validateBearerToken)
 app.use(cors())
 app.use(helmet())
-app.use(validateBearerToken)
 
-app.use(boilerplateRouter)
+app.get('/articles', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+      res.json(articles.map(article => ({
+        id: article.id,
+        title: article.title,
+        style: article.style,
+        content: article.content,
+        date_published: new Date(article.date_published),
+      })))
+    })
+    .catch(next)
+})
+
+
+// app.use(boilerplateRouter)
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
+  res.send('Hello Uncle Leo').status(200)
 })
 
 app.use(errorHandler)
